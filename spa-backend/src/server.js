5afc7e5c072c0535a0079a052638743f80de4372
Dian <ddianapaling04@gmail.com>
2026-03-21 11:42:27 +0800

    server

--- spa-backend/src/server.js
+++ spa-backend/src/server.js
@@ -181,7 +181,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Handle preflight OPTIONS for ALL routes explicitly
-app.options('*', cors(corsOptions));
+app.options('/(.*)', cors(corsOptions));
 
 app.use(express.json());
 app.set('socketio', io);
